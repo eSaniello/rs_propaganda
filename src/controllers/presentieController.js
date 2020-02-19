9dbc97e32c519edf4e41d4c7f7ff96d3984948fe
pebloa <kenson.latchmansing.natin@gmail.com>
2020-02-19 00:28:59 -0300

    working on presentie

---
+++ src/controllers/presentieController.js
@@ -0,0 +1,178 @@
+var globalId;
+var presentieID;
+const tokenPresentie = localStorage.getItem("token");
+
+function getRowId(td) {
+    selectedRow = td.parentElement.parentElement;
+    globalId = selectedRow.cells[0].innerHTML;
+    return globalId;
+}
+
+function token() {
+    var token = localStorage.getItem('token');
+    console.log(token);
+}
+
+function getPresentie() {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + tokenPresentie);
+
+    fetch('http://127.0.0.1:3000/api/presentie', {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default'
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            console.log(data.data.length);
+            if (data.data.length > 0) {
+                var body = "";
+                data.data.forEach(i => {
+                    body += "<tr>";
+                    body += "<td>" + i.presentie_id + "</td>";
+                    body += "<td>" + i.naam + "</td>";
+                    body += "<td>" + i.voornaam + "</td>";
+                    body += `<td> ${i.ressortnaam} / ${i.datum} </td>`;
+                    body += `<td>
+                    <a class='modal-trigger' href='#modal_updatepresentie' title='Wijzigen' data-toggle='tooltip' style='cursor: pointer;' onclick='return getPresentieByID(this)'><i class='small material-icons' style='color: #ffd600;'>edit</i></a>
+                    <a title='Verwijderen' data-toggle='tooltip' style='cursor: pointer;' onclick='return deletePresentie(this)'><i class='small material-icons' style='color: #c62828;'>delete</i></a>
+                </td>`;
+                    body += "</tr>";
+                })
+                document.getElementById('pTableBody').innerHTML = body;
+            }
+        })
+        .catch((err) => console.log(err))
+}
+
+function createPresentie() {
+    let form = document.forms["presentieForm"];
+    let fd = new FormData(form);
+    let data = {};
+    for (let [key, prop] of fd) {
+        data[key] = prop;
+    }
+    VALUE = JSON.stringify(data, null, 4);
+
+    console.log(VALUE);
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + tokenPresentie);
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Accept', 'application/json');
+
+    fetch("http://127.0.0.1:3000/api/presentie", {
+        method: "POST",
+        headers: myHeaders,
+        mode: "cors",
+        cache: "default",
+        body: VALUE
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log('Success', res);
+            location.reload();
+
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+
+    return false;
+}
+
+function deletePresentie(td) {
+    id = getRowId(td);
+
+    if (confirm('Bent u zeker?')) {
+
+        const myHeaders = new Headers();
+        myHeaders.append('Authorization', 'Bearer ' + tokenPresentie);
+
+        fetch('http://127.0.0.1:3000/api/presentie/' + id, {
+            method: 'DELETE',
+            headers: myHeaders,
+            mode: 'cors',
+            cache: 'default'
+        })
+            .then(res => res.json())
+            .then(res => {
+                alert('Presentie succesvol verwijderd!');
+                location.reload();
+            })
+            .catch((err) => console.error(err))
+
+        return false;
+    }
+}
+
+function getPresentieByID(td) {
+    id = getRowId(td);
+    presentieID = id;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + tokenPresentie);
+
+    fetch('http://127.0.0.1:3000/api/presentie/' + id, {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default'
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            console.log(data.success);
+            if (data.success == 1) {
+                naam = data.data.naam;
+                voornaam = data.data.voornaam;
+                email = data.data.email;
+                vergadering = data.data.vergader_id;
+
+                document.getElementById('updateNaam').value = naam;
+                document.getElementById('updateVoornaam').value = voornaam;
+                document.getElementById('updateEmail').value = email;
+                document.getElementById('updateVergadering').value = vergadering;
+            }
+        })
+        .catch((err) => console.log(err))
+    return presentieID;
+}
+
+function updatePresentie() {
+    let form = document.forms["updatePresentieForm"];
+    let fd = new FormData(form);
+    let data = {};
+    for (let [key, prop] of fd) {
+        data[key] = prop;
+    }
+    VALUE = JSON.stringify(data, null, 4);
+
+    console.log(VALUE);
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + tokenPresentie);
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Accept', 'application/json');
+
+    fetch("http://127.0.0.1:3000/api/presentie/" + presentieID, {
+        method: "PUT",
+        headers: myHeaders,
+        mode: "cors",
+        cache: "default",
+        body: VALUE
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log('Success', res);
+            alert('Presentie succesvol gewijzigd!');
+            location.reload();
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+
+    return false;
+}
