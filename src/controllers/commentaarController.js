8dfca4ec88c499241d736455395dec615a598f91
pebloa <kenson.latchmansing.natin@gmail.com>
2020-02-18 23:26:07 -0300

    create commentaar working

--- src/controllers/commentaarController.js
+++ src/controllers/commentaarController.js
@@ -1,46 +1,59 @@
-const tokencm = localStorage.getItem("token");
+const tokenCommentaar = localStorage.getItem("token");
 
-const myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer " + tokencm);
-
-function createComment() {
+function createCommentaar() {
     let form = document.forms["commentaarForm"];
     let fd = new FormData(form);
     let data = {};
     for (let [key, prop] of fd) {
         data[key] = prop;
     }
-    
+
     VALUE = JSON.stringify(data, null, 2);
 
     console.log(VALUE);
 
     const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer " + tokencm);
+    myHeaders.append("Authorization", "Bearer " + tokenCommentaar);
     myHeaders.append('Content-Type', 'application/json');
     myHeaders.append('Accept', 'application/json');
 
-fetch("http://127.0.0.1:3000/api/commentaar/", {
-    method: 'POST',
-    headers: myHeaders,
-    mode: "cors",
-    cache: "default",
-    body: VALUE
-})
-
-    .then(res => res.json())
-    .then(res => {
-        console.log('Success', res);
-        location.reload();
-
-    })
-    .catch((err) => {
-        console.error(err);
+    fetch("http://127.0.0.1:3000/api/commentaar/", {
+        method: 'POST',
+        headers: myHeaders,
+        mode: "cors",
+        cache: "default",
+        body: VALUE
     })
 
-return false;
+        .then(res => res.json())
+        .then(res => {
+            console.log('Success', res);
+            location.reload();
+
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+
+    return false;
 }
+
 function token() {
     var token = localStorage.getItem('token');
     console.log(token);
 }
+
+// function createCommentaar() {
+//     let form = document.forms["commentaarForm"];
+//     let fd = new FormData(form);
+//     let data = {};
+//     for (let [key, prop] of fd) {
+//         data[key] = prop;
+//     }
+
+//     VALUE = JSON.stringify(data, null, 3);
+
+//     console.log(VALUE);
+
+//     return false;
+// }
